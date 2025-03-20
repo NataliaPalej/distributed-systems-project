587f5ed1ba940bf9b99ecf3fd14675a1dc9a3a19
Natalia <PalejNatalia@gmail.com>
2025-03-20 18:49:48 +0000

    Created CorsFixer.java class to fix CORS Policy issues on browser

---
+++ A00279259_Backend/src/main/java/a00279259/CorsFixer.java
@@ -0,0 +1,33 @@
+package a00279259;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class CorsFixer implements Filter {
+	
+	public void init(FilterConfig filterConfig) throws ServletException {
+    }
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");  // Allow all origins
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        chain.doFilter(request, response);
+    }
+	
+	public void destroy() {
+    }
+
+}
